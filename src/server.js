18bd4180f407462f81f103bbe0f6dbc85dbf0917
hackinmonster <eric@fackelman.net>
2025-11-04 10:44:09 -0500

    feat: add base API route structure

--- src/server.js
+++ src/server.js
@@ -2,13 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import locationRoutes from './routes/locationRoutes.js';
+import foodRoutes from './routes/foodRoutes.js';
+import menuRoutes from './routes/menuRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js'
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(cors());
-
 app.use(morgan('tiny'));
-
 app.use(express.json());
+
+app.use('/api/locations', locationRoutes);
+app.use('/api/foods', foodRoutes);
+app.use('/api/menus', menuRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
